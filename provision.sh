a7aaf313284fcb5285f5a91914341fc2a74df64c
Marco Tulio Ávila Cerón <marcotavilac@pm.me>
2024-06-11 11:25:54 +0200

    none improves the script

--- provision.sh
+++ provision.sh
@@ -1,18 +1,38 @@
 #!/usr/bin/env bash
 
+# Check if the script was run with an argument
+if [ $# -eq 1 ]; then
+    SUDO="sudo"
+else
+    SUDO=""
+fi
+
 echo "Provisioning virtual machine..."
 
-dnf update -y
-dnf install -y git
-dnf install -y vim
-dnf install -y wget
-dnf install -y fastfetch
-sudo dnf install -y rpmdevtools rpmlint
-dnf group install -y "MATE Desktop"
-dnf group install -y "MATE Applications"
-localectl set-keymap de
-localectl set-x11-keymap de
-systemctl set-default graphical.target
+#Start by updating the package list
+#and installing some basic packages
+$SUDO dnf update -y
+$SUDO dnf install -y git
+$SUDO dnf install -y vim
+$SUDO dnf install -y wget
+$SUDO dnf install -y fastfetch
 
+#Install the development tools need to create rpms
+$SUDO dnf install -y gcc rpm-build rpm-devel rpmlint make python bash coreutils diffutils patch rpmdevtools
+
+#Install the MATE desktop environment for Fedora and the MATE applications for Fedora.
+#This will take a while and will require a lot of disk space.
+$SUDO dnf group install -y "MATE Desktop"
+$SUDO dnf group install -y "MATE Applications"
+
+#Set the keyboard layout to German
+$SUDO localectl set-keymap de
+$SUDO localectl set-x11-keymap de
+#Uncomment the following line to enable the graphical login
+#systemctl set-default graphical.target
+
+#Display some system information after the end of the installation
 fastfetch
+
+#A message to the user to reboot the virtual machine
 echo "Please reboot the virtual machine to complete the installation."
